8c24eaf7906a23693a9e7503994cd9132e19142c
xiaolongnv123 <3223789869@qq.com>
2017-09-11 23:49:02 +0800

    修改

--- src/com/atguigu/dugujiujian.java
+++ src/com/atguigu/dugujiujian.java
@@ -4,5 +4,9 @@ public class dugujiujian {
 		
 		public static void main(String[] args) {
 			System.out.println("独孤九剑，攒攒攒攒攒攒攒");
+			System.out.println("独孤九剑，攒攒攒攒攒攒攒");
+			System.out.println("独孤九剑，攒攒攒攒攒攒攒");
+			System.out.println("独孤九剑，攒攒攒攒攒攒攒");
+			System.out.println("独孤九剑，攒攒攒攒攒攒攒");
 		}
 }
